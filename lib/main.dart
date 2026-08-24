abeabb51619a88d91201c170c6de1da3d164e656
Fahim007-star <tansanfahim@gmail.com>
2026-08-24 18:28:39 +0600

    New Changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ class BMICalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: InputPage());
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
   }
 }
 
@@ -26,9 +31,9 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(title: Text("BMI CALCULATOR")),
       body: Center(child: Text("Body Text")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      floatingActionButton: Theme(
+        data: ThemeData.light(),
+        child: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
       ),
     );
   }
