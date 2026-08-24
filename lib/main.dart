8e835f110f7ba92a1999289935c3b52373f6f1dd
Fahim007-star <tansanfahim@gmail.com>
2026-08-24 20:07:17 +0600

    New Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator_app/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -14,27 +15,6 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("BMI CALCULATOR")),
-      body: Center(child: Text("Body Text")),
-      floatingActionButton: Theme(
-        data: ThemeData.light(),
-        child: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-      ),
     );
   }
 }
